a54a0c17e35562a95710a4bd0e62f69442dc350d
ABDELLAH JAMAI <abdellahjamai329@gmail.com>
2026-02-04 14:17:43 +0100

    Update 7.change_over_time_analysis.sql

--- Docs of report/7.change_over_time_analysis.sql
+++ Docs of report/7.change_over_time_analysis.sql
@@ -1,1 +1,13 @@
 
+
+-- Analyze sales performence over time --
+select 
+   month(order_date) as order_year,
+   sum(sales_amount) as total_sales,
+   count(distinct customer_key) as total_customers,
+   sum(quantity) as total_quantity
+from Gold.fact_sales
+where order_date is not null
+group by month(order_date)
+order by month(order_date)
+
