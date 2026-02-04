3b0fc76b2a49886d89d40c9e328ae8b7e05d67d4
ABDELLAH JAMAI <abdellahjamai329@gmail.com>
2026-02-04 14:19:54 +0100

    Add category sales analysis SQL query
    Added SQL query to analyze category contributions to overall sales.

--- Docs of report/11.part_to_whole_analysis.sql
+++ Docs of report/11.part_to_whole_analysis.sql
@@ -1,1 +1,20 @@
 
+
+-- Which categories contribute the most to overall sales?
+
+with category_sales as (
+select 
+    category,
+    sum(sales_amount) total_sales
+from Gold.fact_sales f
+left join Gold.dimension_products p
+on p.product_key = f.product_key
+group by category)
+select 
+    category,
+    total_sales,
+    sum (total_sales) over() overall_sales,
+concat(round((cast(total_sales as float) / sum(total_sales) over()) *100, 2), '%') as percentage_of_total
+from category_sales
+order by total_sales desc
+
